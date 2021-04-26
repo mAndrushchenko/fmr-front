b429ce49409f5038bf30ec2f72789af0f2a3759d
Ar4ys <ar4ys@pm.me>
2021-04-26 16:02:11 +0300

    Config eslint, add missing deps

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,11 +1,11 @@
 import { VFC } from 'react'
 import { CssBaseline } from '@material-ui/core'
 
-export const App: VFC = () => {
-  return <>
+export const App: VFC = () => (
+  <>
     <CssBaseline />
     <main>
       <p>Hello world!</p>
     </main>
   </>
-}
+)
