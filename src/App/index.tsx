9be0d3faa55f05cbdfd7e594968979b66e24154a
Andrii Vitrenko <avitrenko2004@gmail.com>
2021-04-26 16:35:25 +0300

    added router

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,11 +1,18 @@
 import { VFC } from 'react'
 import { CssBaseline } from '@material-ui/core'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 export const App: VFC = () => (
   <>
     <CssBaseline />
     <main>
-      <p>Hello world!</p>
+      <Router>
+        <Switch>
+          <Route>
+            
+          </Route>
+        </Switch>
+      </Router>
     </main>
   </>
 )
