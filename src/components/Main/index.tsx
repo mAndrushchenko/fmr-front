20153286dc3de440ee5dd1634cbe4bfee8b90f3d
Andrii Vitrenko <avitrenko2004@gmail.com>
2021-05-05 09:40:36 +0300

    added header

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,14 +1,19 @@
 import { VFC } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Header } from '../Header'
+import { Signin } from '../Signin'
+import { Signup } from '../Signup'
 
 export const Main: VFC = () => (
   <main>
     <Router>
       <Header />
       <Switch>
-        <Route>
-
+        <Route path='/signin'>
+          <Signin />
+        </Route>
+        <Route path='/signup'>
+          <Signup />
         </Route>
       </Switch>
     </Router>
