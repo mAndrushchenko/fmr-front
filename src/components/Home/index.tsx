3ef192c455b185ade5aa1573d9f0c37c1f4ac217
Andrii Vitrenko <avitrenko2004@gmail.com>
2021-05-16 19:24:19 +0300

    added sliders to page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,19 +1,462 @@
+import { Typography } from '@material-ui/core'
 import { VFC } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import { TBook } from 'src/types/store'
 import { CardSlider } from '../Slider'
 
-const styles = makeStyles({
-  root: {
-    padding: '0.5rem'
-  }
-})
+import { styles } from './styles'
 
 export const Home: VFC = () => {
   const classes = styles()
 
+  const newBooks: TBook[] = [
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    }
+  ]
+
+  const personalOfferBooks: TBook[] = [
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    }
+  ]
+
+  const popularBooks: TBook[] = [
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    },
+    {
+      id: 9898,
+      name: 'dfvdf',
+      author: 'dfvdf',
+      genre: 'dvdfv',
+      image: 'https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg',
+      price: 12.99,
+      keywords: [ 'ssdc', 'sdc' ],
+      releaseYear: 2003
+    }
+  ]
+
   return (
     <div className={classes.root}>
-      jhbhjbh
+      <div className={classes.category}>
+        <Typography variant='h6' className={classes.title}>
+          News
+        </Typography>
+        <CardSlider books={newBooks} />
+      </div>
+      <div className={classes.category}>
+        <Typography variant='h6' className={classes.title}>
+          Bestsellers
+        </Typography>
+        <CardSlider books={popularBooks} />
+      </div>
+      <div className={classes.category}>
+        <Typography variant='h6' className={classes.title}>
+          You may also like
+        </Typography>
+        <CardSlider books={personalOfferBooks} />
+      </div>
     </div>
   )
 }
