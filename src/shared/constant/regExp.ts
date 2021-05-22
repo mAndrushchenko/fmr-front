60b2793632d2d6f015e6903731a391682132a6c8
Andrii Vitrenko <avitrenko2004@gmail.com>
2021-05-22 10:02:18 +0300

    chnaged regexp for book name

--- src/shared/constant/regExp.ts
+++ src/shared/constant/regExp.ts
@@ -2,7 +2,7 @@ const regExp = {
   emailRegexp: /^[a-zA-Z]+[0-9]*([.\-_]?[0-9]*[a-zA-Z]+[0-9]*)*@([.\-_]?[0-9]*[a-zA-Z]+[0-9]*)+\.[a-zA-Z]+$/,
   passwordRegexp: /((?=.*[a-z])|(?=.*[а-я])).*((?=.*[A-Z])|(?=.*[А-Я])).*(?=.*\d).*/,
   nameRegexp: /^[a-zA-Z]+(\s[a-zA-Z]+)?$/,
-  bookNameRegexp: /^[a-z0-9]+[.,]?(\s[a-z0-9]+[.,]?)*$/i
+  bookNameRegexp: /^[a-z0-9`']+\s*[`'.,:-]?(\s[a-z0-9'`]+\s*[`'.,:-]?)*$/i
 }
 
 export const {
