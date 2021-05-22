296c02d42b6b2761d314bc47d8650a4efc05b1b6
Andrii Vitrenko <avitrenko2004@gmail.com>
2021-05-22 12:19:04 +0300

    added stepper

---
+++ src/components/Loader/BookInfo/index.tsx
@@ -0,0 +1,21 @@
+import { VFC } from 'react'
+import { useSelector } from 'react-redux'
+import { userSelector } from 'src/store/slices/userSlice'
+
+import Typography from '@material-ui/core/Typography'
+
+import { AdminForm } from '../AdminForm'
+import { UserForm } from '../UserForm'
+
+export const BookInfo: VFC = () => {
+  const { isAdmin, token } = useSelector(userSelector)
+
+  return (
+    <div>
+      <Typography variant='h4'>
+        Upload your book
+      </Typography>
+      {token && (isAdmin ? <AdminForm /> : <UserForm />)}
+    </div>
+  )
+}
--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,10 +1,6 @@
 import { VFC } from 'react'
-import { useSelector } from 'react-redux'
-import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
-import { userSelector } from 'src/store/slices/userSlice'
-import { AdminForm } from './AdminForm'
-import { UserForm } from './UserForm'
+import { BookInfo } from './BookInfo'
 
 const styles = makeStyles({
   root: {
@@ -16,14 +12,10 @@ const styles = makeStyles({
 
 export const Loader: VFC = () => {
   const classes = styles()
-  const { isAdmin, token } = useSelector(userSelector)
 
   return (
     <div className={classes.root}>
-      <Typography variant='h4'>
-        Upload your book
-      </Typography>
-      {token && (isAdmin ? <AdminForm /> : <UserForm />)}
+      <BookInfo />
     </div>
   )
 }
