8a6151ca51b1b37826b4ab87a7ad2ba1addf9b8f
Ar4ys <ar4ys@pm.me>
2021-05-24 14:56:42 +0300

    Update useInterval hook for better UX

--- src/components/Reader/Visualizer/index.tsx
+++ src/components/Reader/Visualizer/index.tsx
@@ -1,52 +1,36 @@
-import { VFC, useRef, useCallback } from 'react'
+import { VFC, useCallback } from 'react'
 import { PauseRounded } from '@material-ui/icons'
 import { useInterval } from '../../../hooks/useInterval'
 import { useStyles } from './styles'
 
 interface VisualizerProps {
   className?: string
-  tokenList: string[]
-  index: number
+  word: string
   speed: number
   fontSize: number
   paused?: boolean
-  onNext: (tokenIndex: number) => void
-  onEnd?: (tokenIndex: number) => void
+  onNext: () => void
   onPause?: (state?: boolean) => void
 }
 
 export const Visualizer: VFC<VisualizerProps> = ({
   className,
-  tokenList,
-  index,
+  word,
   speed,
   fontSize,
   paused = false,
   onNext,
-  onEnd,
   onPause
 }) => {
   const classes = useStyles({ fontSize })
-  const currentToken = tokenList[index]
-  const delayRef = useRef<number>((60 / speed) * 1000)
-  delayRef.current = (60 / speed) * 1000
-
-  useInterval(() => {
-    if (paused) return true
-    if (tokenList.length <= index + 1) {
-      onEnd?.(index)
-      return true
-    }
-
-    onNext?.(++index)
-  }, delayRef, [ paused, onNext ])
-
+  const delay = (60 / speed) * 1000
   const pauseClickHandler = useCallback(() => onPause?.(), [])
+  useInterval(() => onNext?.(), delay, paused)
 
   return (
     <div className={`${classes.root} ${className}`} onClick={pauseClickHandler}>
       {paused && <PauseRounded className={classes.paused} />}
-      <p className={classes.text}>{currentToken}</p>
+      <p className={classes.text}>{word}</p>
     </div>
   )
 }
