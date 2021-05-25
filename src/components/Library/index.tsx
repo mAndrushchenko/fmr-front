857f251554cfc9cfd1ca84fa4001285d50022c4b
mAndrushchenko <andr.mv97@gamil.com>
2021-05-25 10:10:31 +0300

    shop request added

--- src/components/Library/index.tsx
+++ src/components/Library/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { shopSelector } from 'src/store/slices/shopSlice'
+import { VFC, useEffect, useMemo } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getBooksAction, shopSelector } from 'src/store/slices/shopSlice'
+import type { TAppDispatch } from 'src/types/store'
 import { styles } from './styles'
 
-export const Library = () => {
+export const Library: VFC = () => {
   const classes = styles()
-  const { books } = useSelector(shopSelector)
+  const dispatch = useDispatch<TAppDispatch>()
+  const { books, filters } = useSelector(shopSelector)
+  const bookList = useMemo(() => books, [ books ])
+
+  useEffect(() => {
+    if (!bookList.length) {
+      dispatch(getBooksAction(filters))
+    }
+  }, [ dispatch, bookList ])
+
   return (
     <div className={classes.container}>
-      {books.map(book => <div key={book.id}>Book data</div>)}
+      {bookList.map(book => <div key={book.id}>Book data</div>)}
     </div>
   )
 }
